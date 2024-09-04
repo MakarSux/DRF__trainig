4edda2757061f307cf4d6a4ca420975c7bfd3fea
MakarSux <mm.pyshkin@yandex.ru>
2024-09-04 13:41:22 +0300

    Viewsets и ModelViewSet

--- drf/api/views.py
+++ drf/api/views.py
@@ -10,19 +10,25 @@ from .models import *
 from .serializers import *
 
 
-
-class WomenAPIList(generics.ListCreateAPIView):
+from rest_framework import viewsets
+ 
+ 
+class WomenViewSet(viewsets.ModelViewSet):
     queryset = Women.objects.all()
     serializer_class = WomenSerializer
 
+# class WomenAPIList(generics.ListCreateAPIView):
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
 
-class WomenAPIUpdate(generics.UpdateAPIView):
-    queryset = Women.objects.all()
-    serializer_class = WomenSerializer
 
-class WomenAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Women.objects.all()
-    serializer_class = WomenSerializer
+# class WomenAPIUpdate(generics.UpdateAPIView):
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
+
+# class WomenAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
 
 # class WomenAPIView(APIView):
 #     def get(self, request):
