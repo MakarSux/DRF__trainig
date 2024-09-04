9d00d79884ea4eb53681f696b43084ecb3e8bdd8
MakarSux <mm.pyshkin@yandex.ru>
2024-09-04 13:09:35 +0300

    Методы save(), create() и update() класса Serializer

--- drf/drf/urls.py
+++ drf/drf/urls.py
@@ -22,4 +22,5 @@ from api.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/womenlist/', WomenAPIView.as_view()),
+    path('api/v1/womenlist/<int:pk>/', WomenAPIView.as_view()),
 ]
