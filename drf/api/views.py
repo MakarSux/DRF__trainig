4cb430762c80111e3558c90c06654e23fee29949
MakarSux <mm.pyshkin@yandex.ru>
2024-09-04 09:55:22 +0300

    Установка Django Rest Framework

---
+++ drf/api/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+from rest_framework import generics
+
+from .models import *
+from .serializers import *
+
+class WomenAPIView(generics.ListAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
